c9b5a0a3eda457d1630a8edab31ad5cb1f107611
HitBoast <hitblastlive@gmail.com>
2022-08-02 23:42:25 +0600

    Bumped version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open(os.path.join(here, 'requirements.txt'), encoding="utf-8") as f:
     lines = (x.strip() for x in f.read().splitlines())
     requirements = [x for x in lines if x and not x.startswith("#")]
 
-VERSION = '2022.7.19'
+VERSION = '2022.8.2'
 DESCRIPTION = 'An asynchronous Python wrapper for the Minecraft Server Status API.'
 
 # Setup.
