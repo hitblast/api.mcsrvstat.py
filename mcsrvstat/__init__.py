d766656b1f7cba9b20e53d8baf1c1c88cf744f0a
HitBlast <hitblastlive@gmail.com>
2023-09-03 20:16:46 +0600

    updated linting configuration

--- mcsrvstat/__init__.py
+++ mcsrvstat/__init__.py
@@ -24,5 +24,7 @@ SOFTWARE.
 
 
 # Initialize all of the required files.
-from mcsrvstat.main import Server, Base
-from . import exceptions as exceptions, ext as ext
+from mcsrvstat.main import Base, Server
+
+from . import exceptions as exceptions
+from . import ext as ext
