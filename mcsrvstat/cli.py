71390b502e145c7c3d262e243fb3c08fa4dac70a
HitBlast <hitblastlive@gmail.com>
2023-09-03 20:20:05 +0600

    formatted all files properly

--- mcsrvstat/cli.py
+++ mcsrvstat/cli.py
@@ -1,26 +1,4 @@
-'''
-MIT License
-
-Copyright (c) 2022 HitBlast
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
+# SPDX-License-Identifier: MIT
 
 
 # Import built-in modules.
@@ -30,8 +8,8 @@ import asyncio
 import click
 
 # Import local modules.
+from mcsrvstat.ext import Icon, ServerPlatform
 from mcsrvstat.main import Base
-from mcsrvstat.ext import ServerPlatform, Icon
 
 
 # Setting up the default group for Click.
@@ -59,7 +37,7 @@ def fetch(address: str, bedrock: bool, save_icon: bool):
 
     data = loop.run_until_complete(base.fetch_server())
     for key, value in data.items():
-        if not type(value) == dict:
+        if not isinstance(value, dict):
             click.echo(f'{key:<20} {value}')
         else:
             click.echo(f'\n{key:<20}'.upper())
